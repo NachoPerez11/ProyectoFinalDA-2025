2790449dbe9eb6186dba6e57334efe6efa916d3d
Ignacio Perez <nachoo.perez.11@gmail.com>
2025-04-29 19:54:13 -0300

    Listo para ejecutar (launch, index)

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+console.log("Hello from the backend!\n");
+for (let i = 0; i <= 10; i++) {
+    //console.log('5 x ', i, ' = ', 5 * i);		
+    console.log(`5 x ${i} = ${5*i}`);
+}
+console.log("\nThis is a message from the backend.");
