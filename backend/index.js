0b5ba51a176708047af6a9d614b883d2ea9986bc
Ignacio Perez <nachoo.perez.11@gmail.com>
2025-05-13 19:53:07 -0300

    Agregamos logMiddleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { controllers } from './controllers/controllers.js';
 import { errorHandlerMiddleware } from './middlewares/error_handler_middleware.js';
+import { logMiddleware } from './middlewares/log_middleware.js';
 
 const app = express();
+
 app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones como JSON
+app.use(logMiddleware);
 
 controllers(app);
 
