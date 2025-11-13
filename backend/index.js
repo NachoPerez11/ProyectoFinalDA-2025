a7e269691063695655aee4372f32ca66550bca40
Ignacio <nachoo.perez.11@gmail.com>
2025-11-13 16:42:09 -0300

    Arreglo de expresion regular

--- backend/index.js
+++ backend/index.js
@@ -25,11 +25,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const router = express.Router();
 app.use('/api', router);
-app.use('*', express.static(path.join(__dirname, 'dist')));
 
-app.get('/.*/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Captura cualquier ruta y devuelve el index.html
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
 
 configureMiddleware(router);
 configureDependencies();
